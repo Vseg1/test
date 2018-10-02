4ab09385546035ac95b196d53dc28bfe358b9742
Vseg1 <35670761+Vseg1@users.noreply.github.com>
2018-10-02 18:44:36 +0200

    Add files via upload

---
+++ mainclass.java
@@ -0,0 +1,12 @@
+package designpattern;
+
+import org.apache.log4j.Logger;
+
+public class mainclass {
+    private static final Logger LOGGER = Logger.getLogger(mainclass.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.error("Hello World !"); }
+}
+
